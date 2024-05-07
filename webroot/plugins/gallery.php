e869516ffdae7be4bed7f367369696caba5b836b
zitroaen <robertleska@googlemail.com>
2024-05-07 15:35:44 +0200

    Update gallery.php

--- webroot/plugins/gallery.php
+++ webroot/plugins/gallery.php
@@ -53,6 +53,7 @@
 
 
         function loadGalleryImages() {
+            galleryFrame.innerHTML = '';
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
